926b49a54985c8bf6fbd70dc24d4ef9902b344a7
4Sight <116753476+4S1ght@users.noreply.github.com>
2024-08-25 00:55:34 +0200

    ref helpers>objCopyExcept

--- src/Helpers.ts
+++ src/Helpers.ts
@@ -2,8 +2,9 @@
 /**
  * Returns a copy of an object except for the keys specified in `except` field.
  */
-export function objCopyExcept<Obj extends Record<any, any>>(targetObject: Obj, except: (keyof Obj)[]) {
-    const copy = { ...targetObject }
-    except.forEach(key => delete copy[key])
-    return copy
+export function objCopyExcept<Obj extends Record<any, any>, Except extends keyof Obj>
+    (targetObject: Obj, except: Except[]): Omit<Obj, Except> {
+        const copy = { ...targetObject }
+        except.forEach(key => delete copy[key])
+        return copy
 }
