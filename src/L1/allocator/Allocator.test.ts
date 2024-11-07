ab24cd79474a6d6cf6845651f2fc21186fc9d0e3
4Sight <116753476+4S1ght@users.noreply.github.com>
2024-11-07 22:58:19 +0100

    update allocator tests

--- src/L1/allocator/Allocator.test.ts
+++ src/L1/allocator/Allocator.test.ts
@@ -1,12 +1,12 @@
 import { describe, test, expect } from "vitest"
-import AllocSearch from "./AllocSearch.js"
+import Allocator from "./Allocator.js"
 
 describe('MemBlock', async () => {
 
     test('quick consecutive block search', () => {
 
         const source = [0,1,2,3, 1,0,122,123,124,125,126,127, 10,11,12,13,14, 43,44]
-        const get = (size: undefined | number) => AllocSearch.qcbs(source, size)
+        const get = (size: undefined | number) => Allocator.qcbs(source, size)
 
         expect(get(3).items)        .toStrictEqual([0,1,2])
         expect(get(4).items)        .toStrictEqual([0,1,2,3])
