719bf965668e42f7406487b8e5fc6f5d715040a2
4Sight <116753476+4S1ght@users.noreply.github.com>
2025-01-18 18:19:45 +0100

    ~ packageVersion cache

--- src/misc/packageVersion.ts
+++ src/misc/packageVersion.ts
@@ -2,14 +2,19 @@ import fs from 'fs'
 import path from 'path'
 import url from 'url'
 
+let _package: any
+
 export default function semVer() {
+
+    if (_package) return _package
 
     const dirname = path.dirname(url.fileURLToPath(import.meta.url))
     const resource = path.join(dirname, '../../package.json')
     const pack = JSON.parse(fs.readFileSync(resource, 'utf8'))
 
     const [major, minor, patch, tag] = pack.version.split(/\.|-/g)
-    return {
+    
+    return _package = {
         major: Number(major),
         minor: Number(minor),
         patch: Number(patch),
