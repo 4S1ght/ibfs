7f45f4e784365a012105727f7e54e45bd523458a
4Sight <116753476+4S1ght@users.noreply.github.com>
2025-01-25 21:29:30 +0100

    + Finished AdMap & AdStack (untested)

--- src/L1/AdMap.ts
+++ src/L1/AdMap.ts
@@ -1,68 +1,74 @@
 // AdMap (Address Map) is the address space represented by a bitmap
-// used to keep track of allocated and free block addresses in bulk
-// for an entire volume.
+// used to keep track of block allocation of the entire volume.
 
-import { Buffer } from 'buffer'
+import { Buffer } from "node:buffer"
+import IBFSError from "../errors/IBFSError.js"
 
 // Types ==========================================================================================
 
 export interface TAdMapInit {
-    /** The total number of addresses to keep track of.                                     */ addressCount: number
-    /** The offset of the first address to keep track of - must include root & meta blocks. */ offset: number
+    /** The size if the internal address bitmap                       */ size:   number
+    /** The offset used to take N first blocks out of the alloc pool. */ offset: number
 }
 
-// Export =========================================================================================
+// Exports ========================================================================================
 
 export default class AdMap {
 
     public readonly size: number
-    public readonly effectiveSize: number
     public readonly offset: number
 
-    public bitmap: Buffer
+    private readonly bitmap: Buffer
 
     constructor(init: TAdMapInit) {
-        this.size = init.addressCount
+        this.size = init.size
         this.offset = init.offset
-        this.effectiveSize = this.size - this.offset
-        this.bitmap = Buffer.alloc(Math.ceil(this.effectiveSize / 8))
+        this.bitmap = Buffer.alloc(Math.ceil((this.size - this.offset) / 8))
     }
 
-    /**
-     * Checks if an address is allocated
-     * @returns boolean
-     */
-    public get(address: number): boolean {
-        if (address < this.offset || address >= this.effectiveSize) throw new RangeError(`Address [${address}] is out of bounds.`)
-        const byteIndex = Math.floor(address / 8)
-        const bitOffset = address % 8
+    /** 
+     * Translates the physical block address to a bitmap index using the 
+     * configured offset and checks for out of bounds cases.
+    */
+    private translateOffset(address: number): number {
+        const offsetAddress = address - this.offset
+        if (offsetAddress < 0 || offsetAddress >= this.size) {
+            throw new IBFSError(
+                'L1_ALLOC_ADDRESS_OUT_OF_RANGE', 
+                `Address ${address} is out of range.`, 
+                null, 
+                { size: this.size, offset: this.offset }
+            )
+        }
+        return offsetAddress
+    }
+
+    /** Returns `true` if the address is taken. */
+    public isTaken(address: number): boolean {
+        const offsetAddress = this.translateOffset(address)
+        const byteIndex = Math.floor(offsetAddress / 8)
+        const bitOffset = offsetAddress % 8
         return (this.bitmap[byteIndex]! & (1 << bitOffset)) !== 0
     }
 
-    /**
-     * Marks an address as allocated.
-     */
-    public set(address: number) {
-        if (address < this.offset || address >= this.effectiveSize) throw new RangeError(`Address [${address}] is out of bounds.`)
-        const byteIndex = Math.floor(address / 8)
-        const bitOffset = address % 8
+    /** Marks an address as allocated. */
+    protected markAllocated(address: number) {
+        const offsetAddress = this.translateOffset(address)
+        const byteIndex = Math.floor(offsetAddress / 8)
+        const bitOffset = offsetAddress % 8
         this.bitmap[byteIndex]! |= (1 << bitOffset)
     }
 
-    /**
-     * Marks an address as free.
-     */
-    public unset(address: number) {
-        if (address < this.offset || address >= this.effectiveSize) throw new RangeError(`Address [${address}] is out of bounds.`)
-        const byteIndex = Math.floor(address / 8)
-        const bitOffset = address % 8
+    /** Marks an address as free. */
+    protected markFree(address: number) {
+        const offsetAddress = this.translateOffset(address)
+        const byteIndex = Math.floor(offsetAddress / 8)
+        const bitOffset = offsetAddress % 8
         this.bitmap[byteIndex]! &= ~(1 << bitOffset)
     }
 
-    /**
-     * Prints the bitmap as a string of bits for debugging.
-     */
-    toString() {
+    /** Prints the bitmap as a string of bits for debugging. */
+    public toString() {
         return Array.from(this.bitmap)
             .map(byte => byte.toString(2).padStart(8, '0').split('').reverse().join(''))
             .join('')
--- src/L1/AdStack.ts
+++ src/L1/AdStack.ts
@@ -1,83 +1,136 @@
-// AdStack (Address Stack)
-// This module temporarily stores an array of real addresses in a number format
-// that can be easily accessed by the rest of the filesystem driver while the 
-// Address Map stores the rest inside a bitmap to preserve space.
+// AdStack (Address Stack) is responsible for providing a caching
+// Layer on top of the underlying address bitmap. It keeps a small
+// cache of full-size JS number addresses for fast allocation and
+// replenishes it as needed.
 
 // Imports ========================================================================================
 
+import { randomBytes } from "crypto"
 import IBFSError from "../errors/IBFSError.js"
 import AdMap, { TAdMapInit } from "./AdMap.js"
 
 // Types ==========================================================================================
 
 export interface TAdStackInit extends TAdMapInit {
-    /** The size of the address cache that holds fast-access addresses.        */ cacheSize: number
+    /** The number of addresses cached at a time. */ cacheSize: number
 }
 
 // Exports ========================================================================================
 
 export default class AdStack extends AdMap {
 
-    private cache: Array<number>
-    private cacheHealth = 0
+    private readonly cache: Array<number | undefined>
+    private cacheHealth = -1
+    
     private replRegions: number
     private replCycle: Generator<number>
 
     constructor(init: TAdStackInit) {
         super(init)
         this.cache = new Array(init.cacheSize).fill(undefined)
-        this.replRegions = Math.ceil(init.addressCount / init.cacheSize)
+        this.replRegions = Math.ceil(init.size / init.cacheSize)
         this.replCycle = AdStack.createCycle(this.replRegions)
     }
 
-    /** Allocates an address */
-    public alloc() {
+    /**
+     * Allocates an address.  
+     * 
+     * **Note** that this address **MUST** be freed back if it's no
+     * longer used or else it will leak. This will lead to loss
+     * of allocatable disk space until the driver is restarted
+     * and the volume is re-scanned for free space.
+     * 
+     * @returns an address that's been allocated.
+     * @throws L1_ALLOC_ADDRESS_EXHAUSTION
+     */
+    public alloc(): number {
         if (this.cacheHealth === -1) this.fastReplenish()
-        const address = this.cache[this.cacheHealth]
+        const address = this.cache[this.cacheHealth]!
+        this.cache[this.cacheHealth] = undefined
         this.cacheHealth--
         return address
+    } 
+
+    public free(address: number): void {
+        this.markFree(address)
     }
 
-    /** 
-     * Does a basic, quick replenish on the cache.
-     * A full replenish is handled independently in idle time.
+    /**
+     * Replenishes the cache using a quick replenish strategy.
+     * 
+     * The internal bitmap is divided into regions. Only one small region is scanned
+     * at a given time to save time instead of scanning the entire bitmap whenever
+     * the address cache is exhausted and an address is needed.
+     * 
+     * @throws L1_ALLOC_ADDRESS_EXHAUSTION
      */
-    private fastReplenish(retry = 0) {
+    private fastReplenish(retry = 0): void {
 
         // Throw after full cycle of retries to signify address exhaustion
         if (retry === this.replRegions) throw new IBFSError('L1_ALLOC_ADDRESS_EXHAUSTION')
 
-        const regionIndex = this.replCycle.next().value
-        const regionStart = regionIndex * this.cache.length   
-        const regionEnd = Math.min(regionStart + this.cache.length, this.size) - 1
+        const region = this.replCycle.next().value
+        const start = region * this.cache.length
+        const end = Math.min(start + this.cache.length, this.size) - 1
 
-        for (let i = regionStart; i <= regionEnd; i++) {
-            const address = regionStart + i
-            if (this.get(address)) continue
-            this.cache[this.cacheHealth++] = address
+        for (let i = start; i <= end; i++) {
+
+            const address = i + this.offset
+            if (this.isTaken(address)) continue
+
+            // Marks address as allocated to effectively "moves it out"
+            // from the bitmap to the cache
+            this.markAllocated(address)
+            this.cacheHealth++
+            this.cache[this.cacheHealth] = address
+
         }
 
         // Retry a fast replenish with another region if the cache is still empty
-        if (this.cacheHealth === -1) this.fastReplenish(retry++)
+        if (this.cacheHealth === -1) this.fastReplenish(retry+1)
+
     }
 
     /**
-     * Does a full replenish on the cache, scanning the entire
-     * address bitmap if needed.
+     * Does a full scan of the underlying address bitmap to replenish the cache.
+     *
+     * @unused The method is not currently used, but may be in the future.
+     * @throws L1_ALLOC_ADDRESS_EXHAUSTION
      */
-    private replenish() {
+    private fullReplenish() {
+
+        const start = this.offset
+        const end = this.size -1
+
+        for (let i = start; i <= end; i++) {
+            
+            const address = i
+            if (this.isTaken(address)) continue
+
+            // Marks address as allocated to effectively "moves it out"
+            // from the bitmap to the cache
+            this.markAllocated(address)
+            this.cacheHealth++
+            this.cache[this.cacheHealth] = address
+
+            if (this.cacheHealth === this.cache.length - 1) break
+
+        }
+
+        // Throw after full cycle of retries to signify address exhaustion
+        if (this.cacheHealth === -1) throw new IBFSError('L1_ALLOC_ADDRESS_EXHAUSTION')
 
     }
 
     /**
-     * Cycles through chunks of the internal bitmap in order
-     * to separate it into small regions for fast replenishing.
+     * Cycles through regions of the internal bitmap in order
+     * to separate it for faster reads.
      */
     private static *createCycle(range: number): Generator<number> {
         let current = 0
         while (true) {
             yield current
-            current = (current + 1) % (range + 1)
+            current = (current + 1) % range
         }
     }
 
