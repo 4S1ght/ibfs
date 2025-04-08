f8e0bea05dc7550f9944216a782cb0a7d0cac5c8
4Sight <116753476+4S1ght@users.noreply.github.com>
2025-04-08 23:04:10 +0200

    + AdSpaceCache for loading/saving address space bitmap (unfinished)

--- src/L1/alloc/AdSpaceCache.ts
+++ src/L1/alloc/AdSpaceCache.ts
@@ -6,21 +6,48 @@ import crypto       from 'node:crypto'
 import fs           from 'node:fs/promises'
 
 import Memory       from '../../L0/Memory.js'
+import UUID         from '../../misc/uuid.js'
+
+// Types ===============================================================================================================
+
+export type TASCCipher = 'none' | 'aes-128-cbc' | 'aes-256-cbc'
 
 // Exports =============================================================================================================
 
 export default class AdSpaceCache {
     
-    public static async loadFrom(file: string, aesKey: string) {
+    /** 
+     * Loads a cached address space bitmap from the disk.
+     * 
+        Index  | Size | Type   | Description
+        -------|------|--------|-------------------------------------------------------
+        0      | 16B  | Buffer | AES IV
+        16     | 16B  | UUID   | UUID of the associated volume
+        32     | 8B   | Number | Length of the address space bitmap (after decryption)
+        40-128 | ---- | ------ | --------------------- Reserved -----------------------
+        129-N  | N    | Body   | Address space bitmap
+    
+     */
+    public static async loadFrom(file: string, cipher: string, aesKey: string) {
         try {
-            
+
+            const mem = Memory.wrap(await fs.readFile(file))
+
+            const iv    = mem.read(16)
+            const uuid  = UUID.toString(mem.read(16))
+            const size  = mem.readInt64()
+
+            mem.bytesRead = 128
+
+            const data = mem.readRemaining()
+
         } 
         catch (error) {
             
         }
     }
 
-    public static async saveTo(file: string, aesKey: string) {
+    public static async saveTo(file: string, cipher: string,aesKey: string) {
         try {
             
         } 
