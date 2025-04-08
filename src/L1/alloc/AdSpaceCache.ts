6274ad71e1a1ced2025c7d43c1b4013410d0b6e4
4Sight <116753476+4S1ght@users.noreply.github.com>
2025-04-08 21:18:38 +0200

    + AdSpaceCache draft

---
+++ src/L1/alloc/AdSpaceCache.ts
@@ -0,0 +1,33 @@
+// Imports =============================================================================================================
+
+import type * as T  from '../../../types.js'
+
+import crypto       from 'node:crypto'
+import fs           from 'node:fs/promises'
+
+import Memory       from '../../L0/Memory.js'
+
+// Exports =============================================================================================================
+
+export default class AdSpaceCache {
+    
+    public static async loadFrom(file: string, aesKey: string) {
+        try {
+            
+        } 
+        catch (error) {
+            
+        }
+    }
+
+    public static async saveTo(file: string, aesKey: string) {
+        try {
+            
+        } 
+        catch (error) {
+            
+        }
+    }
+
+
+}
