50c11007b472b0e1edc09806b46e4c5975fadd11
4Sight <116753476+4S1ght@users.noreply.github.com>
2025-08-31 16:06:33 +0200

    * Fixed FBM.append early return that prevented final FBM block sync to the disk

--- src/L1/file/FileBlockMap.ts
+++ src/L1/file/FileBlockMap.ts
@@ -129,10 +129,10 @@ export default class FileBlockMap {
         try {
 
             if (addresses.length === 0) return
-
             const startingBlock = this.items.at(-1)!
 
             for (let i = 0; i < addresses.length; i++) {
+
                 const address = addresses[i]!
 
                 // Append address if there's space for it
@@ -147,8 +147,11 @@ export default class FileBlockMap {
                 else {
                     const growError = await this.grow()
                     if (growError) return new IBFSError('L1_FBM_APPEND', null, growError, { iteration })
-                    return await this.append(addresses.slice(i), iteration++)
+
+                    const nestedError =  await this.append(addresses.slice(i), iteration++)
+                    if (nestedError) return new IBFSError('L1_FBM_APPEND', null, nestedError, { iteration })
                 }
+
             }
 
             // Save the last block onto disk if it hasn't been saved yet.
